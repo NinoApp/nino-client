359a5fd2c65afc658046eccb109a6c0e296b2a5b
ebatuhankaynak <ebatuhankaynak@gmail.com>
2019-02-14 12:53:57 +0300

    Remove unused libs

--- scarlet/src/main/java/com/bijoysingh/quicknote/drive/GDriveRemoteDatabase.kt
+++
@@ -1,2 +0,0 @@
-package com.bijoysingh.quicknote.drive
-
--- scarlet/src/main/java/com/bijoysingh/quicknote/firebase/data/TagExtensions.kt
+++
@@ -1,6 +0,0 @@
-package com.bijoysingh.quicknote.firebase.data
-
-import com.maubis.scarlet.base.database.room.tag.Tag
-
-
-fun Tag.getFirebaseTag(): FirebaseTag = FirebaseTag(uuid, title)
