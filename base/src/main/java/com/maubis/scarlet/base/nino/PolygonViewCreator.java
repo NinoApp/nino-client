a7f44c16c3138da5c7448cd624af66570a0e6b2d
batu <ebatuhankaynak@gmail.com>
2019-05-09 12:23:34 +0300

    Fix out of screen points bug

--- base/src/main/java/com/maubis/scarlet/base/nino/PolygonViewCreator.java
+++ base/src/main/java/com/maubis/scarlet/base/nino/PolygonViewCreator.java
@@ -97,6 +97,13 @@ public class PolygonViewCreator {
     }
 
     private void createPolygon(MODE mode, Object dataHolder, Bitmap rgba, ImageView iv){
+        bitmapPos = getBitmapPositionInsideImageView(iv);
+
+        float x = bitmapPos[0];
+        float y = bitmapPos[1];
+        float w = bitmapPos[2];
+        float h = bitmapPos[3];
+
         float origBitWidth = rgba.getWidth();
         float origBitHeight = rgba.getHeight();
         ivWidth = iv.getWidth();
@@ -119,8 +126,8 @@ public class PolygonViewCreator {
                 Log.i("P: POLYGON_POINT", p.x + " @ " + p.y + " @ " + rWidth + " @ " +
                         rHeight + " @ " + ivWidth + " @ " + ivHeight + " @ " + origBitWidth + " @ " + origBitHeight);
 
-                float x = (float) ((p.x * rWidth) - POLYGON_CIRCLE_RADIUS);
-                float y = (float) ((p.y * rHeight) - POLYGON_CIRCLE_RADIUS);
+                x = x + (float) ((p.x * rWidth) - POLYGON_CIRCLE_RADIUS);
+                y = y + (float) ((p.y * rHeight) - POLYGON_CIRCLE_RADIUS);
 
                 Log.i("P: POLYGON_POINT CORNER", x + "-" + y);
                 pointFs.add(new PointF(x, y));
@@ -128,10 +135,10 @@ public class PolygonViewCreator {
         }else{
             Log.i("POLYGON_POINT", "rect v2");
             Rect rect = (Rect) dataHolder;
-            float x = (rect.x * rWidth) - POLYGON_CIRCLE_RADIUS;
-            float y = (rect.y * rHeight) - POLYGON_CIRCLE_RADIUS;
-            float w = (rect.width * rWidth);
-            float h = (rect.height * rHeight);
+            x = x + (rect.x * rWidth) - POLYGON_CIRCLE_RADIUS;
+            y = y + (rect.y * rHeight) - POLYGON_CIRCLE_RADIUS;
+            w = w + (rect.width * rWidth);
+            h = h + (rect.height * rHeight);
 
             pointFs.add(new PointF(x, y));
             pointFs.add(new PointF(x + w, y));
@@ -154,6 +161,10 @@ public class PolygonViewCreator {
 
         orderedPoints = configureOutOfScreenPoints(orderedPoints, ivWidth, ivHeight);
 
+        for (PointF p : orderedPoints.values()){
+            Log.i("P: POLYGON_POINT CONF", p.toString());
+        }
+
         polygonView.setPoints(orderedPoints);
         polygonView.setVisibility(View.VISIBLE);
     }
@@ -169,16 +180,16 @@ public class PolygonViewCreator {
     private Map<Integer, PointF> configureOutOfScreenPoints(Map<Integer, PointF> orderedPoints,
                                                             float width, float height) {
         Map<Integer, PointF> configuredPoints = new HashMap<>();
-        float margin = (float) 0.9;
+        float margin = (float) 0.8;
         int count  = 0;
         for (PointF p: orderedPoints.values()) {
             PointF newP = new PointF(p.x, p.y);
             if((p.x >= width * margin) || (p.y >= height * margin)){
                 if(p.x > width * margin){
-                    newP.x = p.x * margin;
+                    newP.x = width * margin;
                 }
                 if(p.y > height * margin){
-                    newP.y = p.y * margin;
+                    newP.y = height * margin;
                 }
             }
             configuredPoints.put(count, newP);
@@ -201,8 +212,8 @@ public class PolygonViewCreator {
         List<Point> points = new ArrayList<Point>();
 
         for(PointF pf : list){
-            double x = (pf.x + POLYGON_CIRCLE_RADIUS) / rWidth;
-            double y = (pf.y + POLYGON_CIRCLE_RADIUS) / rHeight;
+            double x = (-bitmapPos[0] + pf.x + POLYGON_CIRCLE_RADIUS) / rWidth;
+            double y = (-bitmapPos[1] + pf.y + POLYGON_CIRCLE_RADIUS) / rHeight;
             points.add(new Point(Math.floor(x), Math.floor(y)));
         }
         List<Point> sortedPoints = new ArrayList<Point>();
